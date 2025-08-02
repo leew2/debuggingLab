b7051db068a53d639b424720d279deda6214fd8a
Wang Lee <leew2@csp.edu>
2025-08-02 13:56:59 -0500

    adding random list number

--- debugSelf.py
+++ debugSelf.py
@@ -1,11 +1,19 @@
+
+import pandas as pd
+import numpy as np
+
 def main():
     print("Hello, world!")
-    df = generate_random_dataframe()
+    df = generate_random_dataframe(5, 3) # rows, cols
+    lst = generate_random_list(10, 0, 100) # length, min, max
+    print(lst)
     print(df)
 
-def generate_random_dataframe(rows=5, cols=3):
-    import pandas as pd
-    import numpy as np
+def generate_random_list(length=10, min=0, max=100):
+    lst = np.random.randint(min, max, size=length)
+    return lst.tolist()
+
+def generate_random_dataframe(rows, cols):
     data = np.random.randint(0, 100, size=(rows, cols))
     df = pd.DataFrame(data, columns=[f"data {i+1}" for i in range(cols)])
     return df
